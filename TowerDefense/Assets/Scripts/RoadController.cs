bace197a6b6566a5866367ce430f5dfffb2c0df4
Sohrab <sohrabsn7@yahoo.com>
2021-12-21 13:02:40 +0330

    add soldier base-move

---
+++ TowerDefense/Assets/Scripts/RoadController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour
+{
+    public List<GameObject> soldiers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soldiers = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
