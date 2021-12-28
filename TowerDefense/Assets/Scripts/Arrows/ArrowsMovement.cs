86122d5968a903d83866b4cd59d92cf388982971
Melika Ahmadi Ranjbar <melika.ahmadi2000@yahoo.com>
2021-12-28 16:58:17 +0330

    Attack Okay, Middle Of Drag & Drop

---
+++ TowerDefense/Assets/Scripts/Arrows/ArrowsMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowsMovement : MonoBehaviour
+{
+    public ArrowsConfig config;
+    public Vector3 move;
+
+    void Start()
+    {
+        move = new Vector3(-1, -1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += move;
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Buildings/DragDropMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropMenu : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Debug.Log("ACCESS");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("DOWN");
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BEGIN");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("ON");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("END");
+    }
+
+}
