b7c23caa3ec83841f9b0206ea8375118ebe44a8b
Melika Ahmadi Ranjbar <melika.ahmadi2000@yahoo.com>
2021-12-31 15:54:25 +0330

    Drag Drop Done

--- TowerDefense/Assets/Scripts/Arrows/ArrowsMovement.cs
+++ TowerDefense/Assets/Scripts/Arrows/ArrowsMovement.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ArrowsMovement : MonoBehaviour
 {
     public ArrowsConfig config;
-    public Vector3 move;
+    public float speedFactor;
+    Vector3 move;
+
 
     void Start()
     {
+        speedFactor = 0.5f;
         move = new Vector3(-1, -1, 0);
+        // Rotate randomly when instantiating
+        transform.Rotate(Random.Range(0, 60), 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += move;
+        transform.position += move * speedFactor;
     }
 }
