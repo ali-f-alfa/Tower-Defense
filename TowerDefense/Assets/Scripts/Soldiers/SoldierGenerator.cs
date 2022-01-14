90603d970f4eef0f65e5e44dc4bb706c851ae561
ali-f-alfa <ali.farahat78@gmail.com>
2022-01-14 15:25:23 +0330

    fix scene

--- TowerDefense/Assets/Scripts/Soldiers/SoldierGenerator.cs
+++ TowerDefense/Assets/Scripts/Soldiers/SoldierGenerator.cs
@@ -105,56 +105,7 @@ public class SoldierGenerator : MonoBehaviour
 
         enemy.name = "soldier_" + soldierType.ToString();
         enemy.transform.position = position;
-        enemy.active = true;
 
-        ////SkinnedMeshRenderer SMR = enemy.AddComponent<SkinnedMeshRenderer>();
-        ////SMR = gameObject.GetComponent<SkinnedMeshRenderer>();
-        ////SpriteRenderer spriteRenderer = enemy.AddComponent<SpriteRenderer>();
-        ////var original = gameObject.GetChild(0).gameObject.GetComponent<SkinnedMeshRenderer>();
-        ////var type = original.GetType(); ;
-        //SkinnedMeshRenderer SMR = enemy.AddComponent<SkinnedMeshRenderer>();
-        //SMR.
-        //// Copied fields can be restricted with BindingFlags
-        //System.Reflection.FieldInfo[] fields = type.GetFields();
-        //foreach (System.Reflection.FieldInfo field in fields)
-        //{
-        //    field.SetValue(SMR, field.GetValue(original));
-        //}
-
-        //EnemyScript enemyScript = enemy.AddComponent<EnemyScript>();
-        ////enemyScript.missileMovingSpeed = this.missileMovingSpeed;
-        ////enemyScript.MissileSprite = this.MissileSprite;
-        ////enemyScript.eventSystem = this.eventSystem;
-        ////enemyScript.TypeOfEnemy = soldierType;
-        ////enemyScript.GiftSprit = Gift1Sprit;
-        ////enemyScript.HeartSprit = HeartSprit;
-
-        ////if (soldierType == EnemiesType.Type1)
-        ////{
-        ////    spriteRenderer.sprite = EnemyType1Sprite;
-        ////    enemy.tag = TagNames.EnemyType1.ToString();
-        ////    spriteRenderer.color = new Color(1, 1, 0, 1);
-        ////}
-        ////else if (soldierType == EnemiesType.Type2)
-        ////{
-        ////    spriteRenderer.sprite = EnemyType2Sprite;
-        ////    enemy.tag = TagNames.EnemyType2.ToString();
-        ////    spriteRenderer.color = new Color(0.5f, 0.8f, 0.3f, 1);
-
-        ////}
-        ////else if (soldierType == EnemiesType.Type3)
-        ////{
-        ////    spriteRenderer.sprite = EnemyType3Sprite;
-        ////    enemy.tag = TagNames.EnemyType3.ToString();
-        ////    spriteRenderer.color = new Color(0, 1, 1, 1);
-
-        ////}
-
-
-        ////PolygonCollider2D bc = enemy.AddComponent<PolygonCollider2D>() as PolygonCollider2D;
-        ////bc.isTrigger = true;
-
-        ////enemyRb.velocity = transform.up * -1 * missileMovingSpeed;
         return enemy;
     }
 }
