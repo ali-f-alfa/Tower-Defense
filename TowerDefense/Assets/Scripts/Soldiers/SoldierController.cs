3c89e578b81bf2ca6c0a13493c59af7932058193
ali-f-alfa <ali.farahat78@gmail.com>
2022-01-17 23:57:57 +0330

    add healthBar and death of enemies

--- TowerDefense/Assets/Scripts/Soldiers/SoldierController.cs
+++ TowerDefense/Assets/Scripts/Soldiers/SoldierController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoldierController : MonoBehaviour
 {
@@ -8,23 +9,36 @@ public class SoldierController : MonoBehaviour
     public float moveFactor;
     private Transform target;
     private int wavepointIndex = 0;
-    
+    public Transform HealthBar;
+    private Quaternion HealthBarDefaultRotation;
+
+    public Image GreenHealhBar;
+    public float StartHealth;
+    private float Health;
+
+    private Animator anim;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
+        Health = StartHealth;
+        HealthBarDefaultRotation = HealthBar.rotation;
         state = SoldierStates.MOVE;
         target = Waypoints.points[wavepointIndex];
     }
 
     void Update()
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * moveFactor * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        if (state == SoldierStates.MOVE)
         {
-            GetNextWaypoint();
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * moveFactor * Time.deltaTime, Space.World);
+
+            if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+            {
+                GetNextWaypoint();
+            }
         }
-        
     }
 
     void GetNextWaypoint()
@@ -44,8 +58,26 @@ public class SoldierController : MonoBehaviour
 
         if (dir != Vector3.zero)
         {
-
             transform.forward = dir;
+            HealthBar.transform.rotation = HealthBarDefaultRotation;
+
         }
+    }
+
+    public void TakeDamage (float amount)
+    {
+        Health -= amount;
+        GreenHealhBar.fillAmount = Health / StartHealth;
+        if(Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        state = SoldierStates.DIE;
+        anim.SetInteger("EnemyMode", 2);
+        Destroy(gameObject, 4f);
     }
 }
